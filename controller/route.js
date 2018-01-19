69517713aa1bb758cb7df3414b85162ff3ac508c
pooja <poojamalviya7760@gmail.com>
2018-01-19 18:57:38 +0530

    calculate best route

--- controller/route.js
+++ controller/route.js
@@ -43,10 +43,10 @@ function groupBy() {
     });
 }
 
-// allocate();
+allocate();
 function allocate() {
     return new Promise(function (resolve, reject) {
-        var route = [];
+        var routes = [];
         var routeObj = {};
         var allUser = [];
         var i = 0;
@@ -69,14 +69,16 @@ function allocate() {
                     return reject("not enough cabs");
                 }
                 if (teammemberid.length == cab[i].capacity || (teammemberid.length < cab[i].capacity && isLastItem)) {
-
+                    var routeCost = routeDistance(path);
+                    var bestRoutePath = bestRoute(path);
                     routeObj = {
                         "cab_id": cab[i].id,
                         "team_member_ids": teammemberid,
-                        "route": _.uniq(path)
+                        "route": bestRoutePath,
+                        "route_cost": _.sum(routeCost) // *cab  cost
                     };
 
-                    route.push(routeObj);
+                    routes.push(routeObj);
                     teammemberid = [];
                     path = ["target_headquarter"];
                     i++;
@@ -85,7 +87,7 @@ function allocate() {
             })
             return (sameDrop);
         }).then(function (res) {
-            console.log(route)
+            console.log(routes)
         }).catch(function (err) {
             return reject(err);
         })
@@ -100,10 +102,10 @@ pointC: [ 7, 9, 0, 1, 1 ],
 pointD: [ 2, 2, 2, 0, 1 ],
 pointE: [ 2, 9, 6, 7, 0 ] } 
 */
-routeCost(["target_headquarter", "pointB", "pointC"])//put _.uniq also
-function routeCost(path) {
-
-
+//routeDistance(["target_headquarter", "pointB", "pointC", "pointA"]) 
+function routeDistance(_path) {
+    //console.log(_path,"_path")
+    path = _.uniq(_path);
     var drops = [{
         _id: 'Skcdl804f',
         target_headquarter: [1, 8, 1, 2, 1],
@@ -113,103 +115,93 @@ function routeCost(path) {
         pointD: [2, 2, 2, 0, 1],
         pointE: [2, 9, 6, 7, 0]
     }];
-    var pointA = 0,
-        pointB = 1,
-        pointC = 2,
-        pointD = 3,
-        pointE = 4;
-    var i = 0;
-    var cost;
-    var curr = path[i];
-    console.log(curr)
-    console.log(drops[0].curr)
-    var checkArr = drops[0].target_headquarter;
-    console.log(checkArr, "--")
-    var to = path[i + 1]
-    console.log(to, "to")
-    console.log(checkArr[pointB])
-    cost.push(checkArr[to]);
-    console.log(cost);
+    var map = {
+        "pointA": 0,
+        "pointB": 1,
+        "pointC": 2,
+        "pointD": 3,
+        "pointE": 4
+    };
+    var distance = [];
+    for (i = 0; i <= path.length - 2; i++) {
+        var curr = path[i];
+        var checkArr = drops[0][curr];
+        var to = path[i + 1]
+        var kk = to;
+        var temp = map[kk];
+        var dis = checkArr[temp];
+        distance.push(dis);
+    }
+    return (distance);
 }
 
 
-// bestRoute(["target_headquarter", "pointB", "pointC", "pointD"])
-// function bestRoute(arr) {
-//     db.findAll("DropPoint").then(function (drops) {
-//         console.log(drops[0], "drop")
-//        var pointA = 0,
-//         pointB = 1,
-//         pointC = 2,
-//         pointD = 3,
-//         pointE = 4;
-//         checkArr = drops[0].target_headquarter;
-//     console.log(checkArr[pointA], "---")
-//     var temp;
+//bestRoute(["target_headquarter", "pointB", "pointC", "pointA"])
 
-//     for(i=1; i<=arr.length; i++){
-//         temp= [];
-//         console.log(arr[i])
-//        temp.push(checkArr[arr[i]]);
-//     }
-//     console.log(temp);
-
-//     })
-// }
+function bestRoute(path) {
+    var bestRoute = ["target_headquarter"];
+    tempObj = {};
+    var tempArr = routeDistance(path);
+    for (i = 0; i <= tempArr.length - 1; i++) {
+        tempObj[tempArr[i]] = path[i + 1];
+    }
+    _.forOwn(tempObj, function (value, key) {
+        bestRoute.push(value)
+    });
+    return (bestRoute);
+}
 
 module.exports = router;
 /*
 {
-    "total_cost": "5",
-    "routes": [
-      {
-        "cab_id": "cab1",
-        "team_member_ids": "4,5",
-        "route": "target_headquarter,pointE",
-        "route_cost": 2
-      },
-      {
-        "cab_id": "cab2",
-        "team_member_ids": "3,2,1",
-        "route": "target_headquarter,pointA,pointB,pointC",
-        "route_cost": 3
-      }
-    ]
-   }
-  
+"total_cost": "5",
+"routes": [
+  {
+    "cab_id": "cab1",
+    "team_member_ids": "4,5",
+    "route": "target_headquarter,pointE",
+    "route_cost": 2
+  },
+  {
+    "cab_id": "cab2",
+    "team_member_ids": "3,2,1",
+    "route": "target_headquarter,pointA,pointB,pointC",
+    "route_cost": 3
+  }
+]
+}
+
 
 
 var users = [ { _id: '1',
-    team_member_id: '1',
-    gender: 'M',
-    drop_point: 'pointC' },
-  { _id: '2',
-    team_member_id: '2',
-    gender: 'F',
-    drop_point: 'pointC' },
-  { _id: '4',
-    team_member_id: '4',
-    gender: 'M',
-    drop_point: 'pointC' },
-  { _id: '3',
-    team_member_id: '3',
-    gender: 'M',
-    drop_point: 'pointA' },
-  { _id: '5',
-    team_member_id: '5',
-    gender: 'F',
-    drop_point: 'pointD' },
-  { _id: '6',
-    team_member_id: '6',
-    gender: 'M',
-    drop_point: 'pointD' } ]
+team_member_id: '1',
+gender: 'M',
+drop_point: 'pointC' },
+{ _id: '2',
+team_member_id: '2',
+gender: 'F',
+drop_point: 'pointC' },
+{ _id: '4',
+team_member_id: '4',
+gender: 'M',
+drop_point: 'pointC' },
+{ _id: '3',
+team_member_id: '3',
+gender: 'M',
+drop_point: 'pointA' },
+{ _id: '5',
+team_member_id: '5',
+gender: 'F',
+drop_point: 'pointD' },
+{ _id: '6',
+team_member_id: '6',
+gender: 'M',
+drop_point: 'pointD' } ]
 
 var cab = [ { _id: 'cab2', id: 'cab2', cost: 1, capacity: 3 },
-  { _id: 'cab4', id: 'cab4', cost: 1, capacity: 3 },
-  { _id: 'cab6', id: 'cab6', cost: 1, capacity: 3 },
-  { _id: 'cab1', id: 'cab1', cost: 2, capacity: 2 },
-  { _id: 'cab3', id: 'cab3', cost: 2, capacity: 2 },
-  { _id: 'cab5', id: 'cab5', cost: 2, capacity: 2 } ] 
+{ _id: 'cab4', id: 'cab4', cost: 1, capacity: 3 },
+{ _id: 'cab6', id: 'cab6', cost: 1, capacity: 3 },
+{ _id: 'cab1', id: 'cab1', cost: 2, capacity: 2 },
+{ _id: 'cab3', id: 'cab3', cost: 2, capacity: 2 },
+{ _id: 'cab5', id: 'cab5', cost: 2, capacity: 2 } ] 
 */
-
-
-
